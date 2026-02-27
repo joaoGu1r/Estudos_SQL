0d1bfad46d35f549379a75d6cec0f0c0043d673d
Joao Guiherme <joao.guilhermesilva@ufrpe.br>
2026-02-27 10:19:44 -0300

    Analisando dados no sql

---
+++ analise_de_dados.sql
@@ -0,0 +1,38 @@
+select *from cursos
+order by nome asc;
+select *from cursos
+order by carga asc;
+
+select *from cursos
+order by nome desc;
+
+
+select nome, carga, ano from cursos
+order by ano, nome desc; 
+
+select * from cursos
+where ano = '2017'
+order by nome;
+
+select nome , descricao, ano from cursos
+where ano != '2017'
+order by ano , nome;
+
+select nome, ano from cursos
+where ano between '2017' and '2024'
+order by ano desc, nome asc;
+
+
+select nome, descricao, ano from cursos
+where ano in (2017 , 2024)
+order by ano desc, nome asc;
+
+select * from cursos
+where  carga > 35 and totaulas < 30
+order by ano desc, nome asc;
+
+select * from cursos
+where  carga < 35 or totaulas < 30
+order by ano desc, nome asc;
+
+
